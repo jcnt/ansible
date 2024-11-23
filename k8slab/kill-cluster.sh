8b2c7eacba3322198fa3dd5bb38fd3e97ffb9ba4
Jacint Juhasz <yyaazz@gmail.com>
2024-11-23 18:59:19 +0000

    new kill-cluster: force parallel halt of all nodes

--- k8slab/kill-cluster.sh
+++ k8slab/kill-cluster.sh
@@ -2,9 +2,35 @@
 
 source /home/jacint/.vcps
 
-if [[ $# -eq 0 ]]
-    then echo paramter: cluster[1,2,3]
-else
-    for i in `cat $1`; do ansible-playbook -f 30 /home/jacint/ansible/vmware/poweroff-force.yaml --extra-vars "vmname=$i password=$PS"; done
-fi
 
+if [[ $# -eq 0 ]]
+    then echo paramter: cluster[1..3]
+else
+    if [[ $1 == cluster1 ]]
+        then 
+            master=jjmaster
+            w1=jjkw11
+            w2=jjkw12
+            w3=jjkw13
+            w4=jjkw14
+    
+    elif [[ $1 == cluster2 ]]
+        then 
+            master=jjmastdr
+            w1=jjkw21
+            w2=jjkw22
+            w3=jjkw23
+            w4=jjkw24
+    
+    elif [[ $1 == cluster3 ]]
+        then 
+            master=jjmast3r
+            w1=jjkw31
+            w2=jjkw32
+            w3=jjkw33
+            w4=jjkw34
+    fi
+
+    ansible-playbook /home/jacint/ansible/vmware/poweroff-force.yaml -e "master=$master" -e "w1=$w1" -e "w2=$w2" -e "w3=$w3" -e "w4=$w4" -e "password=$PS"
+
+fi
