103652490cde52ffbab48ad40e631f8fc9d04a14
Jacint Juhasz <yyaazz@gmail.com>
2024-11-24 21:02:16 +0000

    Lazy commit Sun Nov 24 21:02:16 UTC 2024

---
+++ k8slab/vm-on.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /home/jacint/.vcps
+
+if [[ $# -eq 0 ]]
+    then echo paramter: vmname
+else
+    ansible-playbook /home/jacint/ansible/vmware/poweron.yaml --extra-vars "vmname=$1 password=$PS"
+fi
+
