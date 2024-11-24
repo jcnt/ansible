d72305470bbf2c2b321a3adce4c535a029451a4b
Jacint Juhasz <yyaazz@gmail.com>
2024-11-24 21:26:37 +0000

    Lazy commit Sun Nov 24 21:26:37 UTC 2024

--- k8slab/getvms.sh
+++ k8slab/getvms.sh
@@ -2,7 +2,4 @@
 
 source /home/jacint/.vcps
 
-ansible-playbook /home/jacint/ansible/vmware/getvms.yaml -e "vmname=$vmname" -e "password=$PS"
-
-
-
+ansible-playbook /home/jacint/ansible/vmware/getvms.yaml -e "password=$PS" |egrep  '(guest_name|power_state)'
