5637049b8e24e68a1c6d55d9064c0e329c0269e2
Jacint Juhasz <yyaazz@gmail.com>
2025-01-21 00:51:40 +0100

    Lazy commit Tue Jan 21 12:51:40 AM CET 2025

--- k8slab/new-cluster-deploy.sh
+++ k8slab/new-cluster-deploy.sh
@@ -2,7 +2,7 @@
 source /home/jacint/.vcps
 
 if [[ $# -eq 0 ]]
-    then echo paramter: [cluster[1..5] ] [version, e.g. 1.29.8]
+    then echo paramter: [cluster[1..5] ] [version, e.g. 1.29.8] [px: optonal to install px]
     exit 1
 fi
 
@@ -12,42 +12,36 @@ if [[ $1 == cluster1 ]]
     then 
         master=m1
         workers="w11 w12 w13"
-	    podcidr=10.77.1.0/24
-	    svccidr=10.78.1.0/24
+	    svccidr=10.78.10.0/24
 
 elif [[ $1 == cluster2 ]]
     then 
 	    master=m2
         workers="w21 w22 w23"
-	    podcidr=10.77.2.0/24
 	    svccidr=10.78.2.0/24
 
 elif [[ $1 == cluster3 ]]
     then 
 	    master=m3
         workers="w31 w32 w33"
-	    podcidr=10.77.3.0/24
 	    svccidr=10.78.3.0/24
 
 elif [[ $1 == cluster4 ]]
     then 
 	    master=m4
         workers="w41 w42 w43"
-	    podcidr=10.77.5.0/24
 	    svccidr=10.78.5.0/24
 
 elif [[ $1 == cluster5 ]]
     then 
 	    master=m5
         workers="w51 w52 w53"
-	    podcidr=10.77.6.0/24
 	    svccidr=10.78.6.0/24
 
 elif [[ $1 == clusterbk ]]
     then 
 	    master=jjpxbkm
         workers="jjpxbk1 jjpxbk2 jjpxbk3"
-	    podcidr=10.77.4.0/24
 	    svccidr=10.78.4.0/24
 fi
 
@@ -62,8 +56,8 @@ for i in `cat $1`;
     do ansible-playbook /home/jacint/ansible/vmware/poweron.yaml -e "vmname=$i" -e "password=$PS"; 
 done
 
-echo "sleeping for 30 sec, VMs coming up, re-IP, SSH re-key"
-sleep 30
+echo "sleeping for 10 sec, VMs coming up, re-IP, SSH re-key"
+sleep 15
 
 # finalize cluster nodes
 for i in `cat $1`; 
@@ -77,34 +71,26 @@ ansible-playbook init-containerd.yaml -e "cluster=$1"
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$debver/deb/ /" > kubernetes.sources
 
 ansible-playbook init-kubeadm.yaml -e "cluster=$1" -e "version=$2" -e "debver=$debver"
-ssh $master sudo kubeadm init --pod-network-cidr $podcidr --service-cidr $svccidr
+ssh $master sudo kubeadm init --service-cidr $svccidr
 ansible-playbook jump-get-kube-config.yaml --extra-vars "master=$master"
 mv /tmp/$master/etc/kubernetes/admin.conf /home/jacint/.kube/config.$1
 cp /home/jacint/.kube/config.$1 /home/jacint/.kube/config
-# kubectl apply -f flannel-$1.yaml
 kubectl apply -f https://reweave.azurewebsites.net/k8s/v$debver/net.yaml
 
 TOKEN=`ssh $master sudo kubeadm token list |grep "kubeadm init" |awk '{print $1}'`
 for i in `echo $workers`;do ssh $i sudo kubeadm join $master:6443 --token $TOKEN --discovery-token-unsafe-skip-ca-verification; done
 ansible-playbook distribute-kube-config.yaml --extra-vars "cluster=$1"
 
-if [[ $1 == cluster1 ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.1.0/24"}}'; done
-elif [[ $1 == cluster2 ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.2.0/24"}}'; done
-elif [[ $1 == cluster3 ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.3.0/24"}}'; done
-elif [[ $1 == clusterbk ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.4.0/24"}}'; done
-elif [[ $1 == cluster4 ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.5.0/24"}}'; done
-elif [[ $1 == cluster5 ]]
-    then
-        for i in `echo $workers`;do kubectl patch node $i -p '{"spec":{"podCIDR":"10.77.6.0/24"}}'; done
+if [[ $3 == px ]]
+    then 
+        echo 
+        echo "Installing Portworx"
+        echo
+        kubectl apply -f ~/locyml/3.2-px-operator.yaml
+        kubectl apply -f ~/locyml/vsphere-px.yaml
+        sleep 60
+        kubectl apply -f ~/locyml/3.2-px-sc-clddrv.yaml
+        ssh m1 touch .px
 fi
+
 
