484f0f220b87ece369fac1a793b065a9b1dec23e
Jacint Juhasz <yyaazz@gmail.com>
2025-05-27 14:02:59 +0200

    Lazy commit Tue May 27 02:02:59 PM CEST 2025

--- acdemo/ac-py.py
+++ acdemo/ac-py.py
@@ -7,39 +7,31 @@ from pypureclient import flasharray
 
 urllib3.disable_warnings()
 
-X70A = "10.206.205.33"
-X70A_TOKEN = 
+tdb = {}
 
-X70B = "10.206.205.36"
-X70B_TOKEN =
+with open("/home/jacint/.fatoken") as token:
+    lines = token.readlines()
 
-x70a = flasharray.Client(X70A, api_token=X70A_TOKEN, verify_ssl=False)
-x70b = flasharray.Client(X70B, api_token=X70B_TOKEN, verify_ssl=False)
+for line in lines:
+    ll = line.strip().split("=")
+    tdb[ll[0]] = ll[1]
 
-# x70a = flasharray.Client(
-#    "10.206.205.33",
-#    private_key_file="/home/jacint/.fassl/fa2xprivate.pem",
-#    private_key_password="",
-#    username="jacint",
-#    x70a_id="bfc23d01-3f5d-47f7-b40e-b1951f8aff18",
-#    key_id="55831b7e-b4c0-4e3d-b701-58cbb47b9d07",
-#    issuer="jjPython",
-# )
-
+x70a = flasharray.Client("10.206.205.33", api_token=tdb["X70A"], verify_ssl=False)
+x70b = flasharray.Client("10.206.205.36", api_token=tdb["X70B"], verify_ssl=False)
 
 pod = flasharray.PodPost()
 response = x70a.post_pods(pod=pod, names=["jjpy"])
-print(response.status_code)
+print("Creating Pod...", response.status_code)
 
 response = x70a.post_pods_arrays(member_names=["PureCZ-X70-B"], group_names=["jjpy"])
-print(response.status_code)
+print("Adding Pod member...", response.status_code)
 
 vol = flasharray.VolumePost(provisioned=100 * 1024 * 1024 * 1024)
 response = x70a.post_volumes(volume=vol, names=["jjpy::jjpy"])
-print(response.status_code)
+print("Creating Vol...", response.status_code)
 
 response = x70a.post_connections(volume_names=["jjpy::jjpy"], host_names=["jjRHEL"])
-print(response.status_code)
+print("Connecting Vol on first array...", response.status_code)
 
 response = x70b.get_volumes(names=["jjpy::jjpy"])
 
@@ -54,4 +46,4 @@ for i in range(10):
     time.sleep(1)
 
 response = x70b.post_connections(volume_names=["jjpy::jjpy"], host_names=["jjRHEL"])
-print(response.status_code)
+print("Connecting Vol on second array...", response.status_code)
